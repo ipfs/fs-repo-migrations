2901babcfdf660e9380668141d559fe21d44dca8
Christian Couder <chriscool@tuxfamily.org>
2016-01-22 16:46:37 +0100

    sharness: add pin migration test

--- sharness/lib/test-lib.sh
+++ sharness/lib/test-lib.sh
@@ -51,10 +51,24 @@ stop_docker() {
 # Echo the args, run the cmd, and then also fail,
 # making sure a test case fails.
 test_fsh() {
-    echo "> $@"
-    eval "$@"
-    echo ""
-    false
+	echo "> $@"
+	eval "$@"
+	echo
+	false
+}
+
+# Same as sharness' test_cmp but using test_fsh (to see the output).
+# We have to do it twice, so the first diff output doesn't show unless it's
+# broken.
+test_cmp() {
+	diff -q "$@" >/dev/null || test_fsh diff -u "$@"
+}
+
+# Same as test_cmp above, but we sort files before comparing them.
+test_sort_cmp() {
+	sort "$1" >"$1_sorted" &&
+	sort "$2" >"$2_sorted" &&
+	test_cmp "$1_sorted" "$2_sorted"
 }
 
 LOCAL_IPFS_UPDATE="../bin/ipfs-update"
