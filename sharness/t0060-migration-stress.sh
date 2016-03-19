849cc2659c63327264193e9869bdfb14dc939a4f
Jeromy <jeromyj@gmail.com>
2016-03-19 00:21:33 -0700

    heavy migration stress tests

--- sharness/t0060-migration-stress.sh
+++ sharness/t0060-migration-stress.sh
@@ -34,23 +34,12 @@ test_docker_wait_for_file() {
 
 test_install_version "v0.3.11"
 
-test_expect_success "'ipfs init' succeeds" '
-	export IPFS_PATH=/root/.ipfs &&
-	exec_docker "$DOCID" "IPFS_PATH=$IPFS_PATH BITS=2048 ipfs init" >actual 2>&1 ||
-	test_fsh cat actual
-'
+test_init_daemon "$DOCID"
 
-test_expect_success "'ipfs daemon' succeeds" '
-	exec_docker "$DOCID" "ipfs daemon >actual_daemon 2>daemon_err &"
-'
-
-test_expect_success "api file shows up" '
-	test_docker_wait_for_file "$DOCID" 20 100ms "$IPFS_PATH/api"
-'
+test_start_daemon "$DOCID"
 
 test_expect_success "make a couple files" '
-	#drun "random-files -depth=6 -dirs=7 -files=10 manyfiles" > filenames
-	drun "$GUEST_RANDOM_FILES -depth=3 -dirs=3 -files=10 manyfiles" > filenames
+	drun "$GUEST_RANDOM_FILES -depth=6 -dirs=7 -files=10 manyfiles" > filenames
 '
 
 test_expect_success "add a few files" '
@@ -82,24 +71,104 @@ test_expect_success "pin some objects recursively" '
 '
 
 test_expect_success "pin some objects directly" '
-	pin_hashes recurpins "-r=false"
+	pin_hashes directpins "-r=false"
 '
 
 test_expect_success "get full ref list" '
-	drun "ipfs refs local" > start_refs
+	drun "ipfs refs local" | sort > start_refs
 '
 
 test_expect_success "get pin lists" '
-	drun "ipfs pin ls --type=recursive" > start_rec_pins &&
-	drun "ipfs pin ls --type=direct" > start_dir_pins &&
-	drun "ipfs pin ls --type=indirect" > start_ind_pins
+	drun "ipfs pin ls --type=recursive" | sort > start_rec_pins &&
+	drun "ipfs pin ls --type=direct" | sort > start_dir_pins &&
+	drun "ipfs pin ls --type=indirect" | sort > start_ind_pins
 '
 
-# STOP DAEMON
+test_stop_daemon $DOCID
 
-# UPGRADE TO 0.4.0 AND RUN MIGRATION
+test_install_version "v0.4.0-dev"
 
-# VERIFY EVERYTHING
+test_start_daemon $DOCID
+
+test_expect_success "list all refs after migration" '
+	drun "ipfs refs local" | sort > after_refs
+'
+
+test_expect_success "list all pins after migration" '
+	drun "ipfs pin ls --type=recursive" | sort > after_rec_pins &&
+	drun "ipfs pin ls --type=direct" | sort > after_dir_pins &&
+	drun "ipfs pin ls --type=indirect" | sort > after_ind_pins
+'
+
+test_expect_success "refs look right" '
+	comm -23 start_refs after_refs > missing_refs &&
+	touch empty_refs_file &&
+	test_cmp missing_refs empty_refs_file
+'
+
+test_expect_success "pins all look the same" '
+	test_cmp start_rec_pins after_rec_pins &&
+	test_cmp start_dir_pins after_dir_pins &&
+	test_cmp start_ind_pins after_ind_pins
+'
+
+test_expect_success "manually compute gc set" '
+	cat after_rec_pins after_dir_pins after_ind_pins | sort > all_pinned
+'
+
+test_expect_success "run a gc" '
+	drun "ipfs repo gc" | sort > gc_out	
+'
+
+test_expect_success "no pinned objects were gc'ed" '
+	comm -12 gc_out all_pinned > gced_pinned_objects &&
+	test_cmp empty_refs_file gced_pinned_objects
+'
+
+test_expect_success "list all pins after gc" '
+	drun "ipfs pin ls --type=recursive" | sort > gc_rec_pins &&
+	drun "ipfs pin ls --type=direct" | sort > gc_dir_pins &&
+	drun "ipfs pin ls --type=indirect" | sort > gc_ind_pins
+'
+
+test_expect_success "pins all look the same" '
+	test_cmp start_rec_pins gc_rec_pins &&
+	test_cmp start_dir_pins gc_dir_pins &&
+	test_cmp start_ind_pins gc_ind_pins
+'
+
+test_expect_success "fetch all refs" '
+	drun "ipfs refs local" | sort | uniq > post_gc_refs
+'
+
+first_elems() {
+	cat "$1" | awk '{ print $1 }'
+}
+
+test_expect_success "get just hashes of pins" '
+	first_elems all_pinned | sort | uniq > all_pinned_refs
+'
+
+test_stop_daemon $DOCID
+
+test_can_fetch_buggy_hashes() {
+	ref_file="$1"
+	for ref in `cat $ref_file`; do
+		if ! drun "ipfs block get $ref" > /dev/null; then
+			echo "FAILURE: $ref"
+			return 1
+		fi
+	done
+}
+
+# the refs we're testing here are a result of the go-datastore path bug.
+# The byte representation of the hash ends in a '/' character, which gets trimmed
+# by go-datastore. This does not imply that there is any data lost, just that
+# we cant enumerate those refs properly
+test_expect_success "no pinned objects are missing from local refs" '
+	comm -23 all_pinned_refs post_gc_refs > missing_pinned_objects &&
+	test_can_fetch_buggy_hashes missing_pinned_objects
+'
 
 test_expect_success "stop docker container" '
 	stop_docker "$DOCID"
