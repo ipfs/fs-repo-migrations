ceac53d2d06af65fbdea8db792980d0d36628953
Dirk McCormick <dirkmdev@gmail.com>
2020-05-22 11:35:06 -0400

    feat: migration 9 to 10

---
+++ ipfs-9-to-10/migration/config_conv.go
@@ -0,0 +1,181 @@
+package mg9
+
+import (
+	"encoding/json"
+	"io"
+	"io/ioutil"
+	"os"
+	"regexp"
+	"strings"
+
+	log "github.com/ipfs/fs-repo-migrations/stump"
+)
+
+var (
+	ip4BootstrapAddr  = "/ip4/104.131.131.82/tcp/4001/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
+	quicBootstrapAddr = "/ip4/104.131.131.82/udp/4001/quic/p2p/QmaCpDMGvV2BGHeYERUEnRQAwe3N8SzbUtfsmvsqQLuvuJ"
+)
+
+// convArray does an inplace conversion of an array of strings
+// configuration from one version to another
+type convArray func([]string) []string
+
+// convertFile converts a config file from one version to another
+func convertFile(orig string, new string, quicEnabled bool, convBootstrap convArray, convSwarm convArray) error {
+	in, err := os.Open(orig)
+	if err != nil {
+		return err
+	}
+	out, err := os.Create(new)
+	if err != nil {
+		return err
+	}
+	return convert(in, out, quicEnabled, convBootstrap, convSwarm)
+}
+
+// convert converts the config from one version to another
+func convert(in io.Reader, out io.Writer, quicEnabled bool, convBootstrap convArray, convSwarm convArray) error {
+	data, err := ioutil.ReadAll(in)
+	if err != nil {
+		return err
+	}
+	confMap := make(map[string]interface{})
+	if err = json.Unmarshal(data, &confMap); err != nil {
+		return err
+	}
+
+	// Convert bootstrap config
+	convertBootstrap(confMap, convBootstrap)
+
+	// Convert swarm config
+	if quicEnabled {
+		// When enabling quic
+		// - Remove experimental option from config
+		confEnabled, ok := removeExperimentalQuic(confMap)
+		// - Only convert swarm config if experimental quic option was present
+		//   and not enabled
+		if ok && !confEnabled {
+			convertSwarm(confMap, convSwarm)
+		}
+	} else {
+		convertSwarm(confMap, convSwarm)
+	}
+
+	fixed, err := json.MarshalIndent(confMap, "", "  ")
+	if err != nil {
+		return err
+	}
+	out.Write(fixed)
+	out.Write([]byte("\n"))
+	return nil
+}
+
+func removeExperimentalQuic(confMap map[string]interface{}) (bool, bool) {
+	confExpi := confMap["Experimental"].(map[string]interface{})
+	if confExpi == nil {
+		return false, false
+	}
+	enabledi, ok := confExpi["QUIC"]
+	enabled := enabledi.(bool)
+
+	delete(confExpi, "QUIC")
+
+	return enabled, ok
+}
+
+func convertBootstrap(confMap map[string]interface{}, conv convArray) {
+	bootstrapi, _ := confMap["Bootstrap"].([]interface{})
+	if bootstrapi == nil {
+		log.Log("Bootstrap field missing or of the wrong type")
+		return
+	}
+	bootstrap := make([]string, len(bootstrapi))
+	for i := range bootstrapi {
+		bootstrap[i] = bootstrapi[i].(string)
+	}
+	confMap["Bootstrap"] = conv(bootstrap)
+}
+
+func convertSwarm(confMap map[string]interface{}, conv convArray) {
+	addressesi, _ := confMap["Addresses"].(map[string]interface{})
+	if addressesi == nil {
+		log.Log("Addresses field missing or of the wrong type")
+		return
+	}
+
+	swarmi, _ := addressesi["Swarm"].([]interface{})
+	if swarmi == nil {
+		log.Log("Addresses.Swarm field missing or of the wrong type")
+		return
+	}
+
+	swarm := make([]string, len(swarmi))
+	for i := range swarmi {
+		swarm[i] = swarmi[i].(string)
+	}
+	addressesi["Swarm"] = conv(swarm)
+}
+
+func ver9to10Bootstrap(bootstrap []string) []string {
+	res := make([]string, 0, len(bootstrap)+1)
+	for _, addr := range bootstrap {
+		res = append(res, addr)
+
+		// If the config has the old IP v4 bootstrapper, add the new QUIC
+		// bootstrapper
+		if addr == ip4BootstrapAddr {
+			res = append(res, quicBootstrapAddr)
+		}
+	}
+
+	return res
+}
+
+func ver10to9Bootstrap(bootstrap []string) []string {
+	res := make([]string, 0, len(bootstrap))
+	for _, addr := range bootstrap {
+		// If the config has the new IP v4 bootstrapper, remove it
+		if addr != quicBootstrapAddr {
+			res = append(res, ip4BootstrapAddr)
+		}
+	}
+
+	return res
+}
+
+var tcpRegexp = regexp.MustCompile(`\/tcp\/[0-9]+\/`)
+
+func ver9to10Swarm(swarm []string) []string {
+	res := make([]string, 0, len(swarm)*2)
+	for _, addr := range swarm {
+		res = append(res, addr)
+
+		// If the old configuration already has a quic address in it, assume
+		// the user has already set up their swarm addresses for quic and leave
+		// things as they are
+		if strings.Contains(addr, "/udp/quic") {
+			return swarm
+		}
+	}
+
+	// For each tcp address, add a corresponding quic address
+	for _, addr := range swarm {
+		if tcpRegexp.MatchString(addr) {
+			res = append(res, tcpRegexp.ReplaceAllString(`\/tcp\/([0-9]+)\/`, `\/udp\/$1\/quic`))
+		}
+	}
+
+	return res
+}
+
+func ver10to9Swarm(swarm []string) []string {
+	// Remove quic addresses
+	res := make([]string, 0, len(swarm))
+	for _, addr := range swarm {
+		if !strings.Contains(addr, "/udp/quic") {
+			res = append(res, addr)
+		}
+	}
+
+	return res
+}
---
+++ ipfs-9-to-10/migration/migration.go
@@ -0,0 +1,146 @@
+package mg9
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strconv"
+
+	migrate "github.com/ipfs/fs-repo-migrations/go-migrate"
+	lock "github.com/ipfs/fs-repo-migrations/ipfs-1-to-2/repolock"
+	mfsr "github.com/ipfs/fs-repo-migrations/mfsr"
+	log "github.com/ipfs/fs-repo-migrations/stump"
+)
+
+type Migration struct{}
+
+func (m Migration) Versions() string {
+	return "9-to-10"
+}
+
+func (m Migration) Reversible() bool {
+	return true
+}
+
+func (m Migration) Apply(opts migrate.Options) error {
+	log.Verbose = opts.Verbose
+	log.Log("applying %s repo migration", m.Versions())
+
+	log.VLog("locking repo at %q", opts.Path)
+	lk, err := lock.Lock2(opts.Path)
+	if err != nil {
+		return err
+	}
+	defer lk.Close()
+
+	repo := mfsr.RepoPath(opts.Path)
+
+	log.VLog("  - verifying version is '9'")
+	if err := repo.CheckVersion("9"); err != nil {
+		return err
+	}
+
+	basepath := filepath.Join(opts.Path, "config")
+	v9path := filepath.Join(opts.Path, "config-v9")
+	if err := os.Rename(basepath, v9path); err != nil {
+		if os.IsNotExist(err) {
+			_, err2 := os.Stat(v9path)
+			if err2 == nil {
+				log.Log("... config already renamed to config-v9, continuing")
+				err = nil
+			}
+		}
+		if err != nil {
+			return err
+		}
+	}
+
+	log.Log("> Upgrading config to new format")
+
+	if err := convertFile(v9path, basepath, true, ver9to10Bootstrap, ver9to10Swarm); err != nil {
+		if opts.NoRevert {
+			return err
+		}
+		err := os.Rename(v9path, basepath)
+		if err != nil {
+			log.Error(err)
+		}
+		return err
+	}
+
+	if err := repo.WriteVersion("10"); err != nil {
+		log.Error("failed to update version file to 10")
+		return err
+	}
+
+	log.Log("updated version file")
+
+	return nil
+}
+
+func writePhase(file string, phase int) error {
+	return ioutil.WriteFile(file, []byte(fmt.Sprint(phase)), 0666)
+}
+
+func readPhase(file string) (int, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		if os.IsNotExist(err) {
+			return 0, nil
+		}
+		return 0, err
+	}
+
+	return strconv.Atoi(string(data))
+}
+
+func (m Migration) Revert(opts migrate.Options) error {
+	log.Verbose = opts.Verbose
+	log.Log("reverting migration")
+	lk, err := lock.Lock2(opts.Path)
+	if err != nil {
+		return err
+	}
+	defer lk.Close()
+
+	repo := mfsr.RepoPath(opts.Path)
+	if err := repo.CheckVersion("10"); err != nil {
+		return err
+	}
+
+	phasefile := filepath.Join(opts.Path, "revert-phase")
+	basepath := filepath.Join(opts.Path, "config")
+	v10path := filepath.Join(opts.Path, "config-v10")
+
+	phase, err := readPhase(phasefile)
+	if err != nil {
+		return fmt.Errorf("reading revert phase: %s", err)
+	}
+
+	for ; phase < 4; phase++ {
+		switch phase {
+		case 0:
+			if err := os.Rename(basepath, v10path); err != nil {
+				return err
+			}
+		case 1:
+			if err := convertFile(v10path, basepath, false, ver10to9Bootstrap, ver10to9Swarm); err != nil {
+				return err
+			}
+		case 2:
+			if err := repo.WriteVersion("9"); err != nil {
+				return err
+			}
+			if opts.Verbose {
+				fmt.Println("lowered version number to 9")
+			}
+		}
+		if err := writePhase(phasefile, phase+1); err != nil {
+			return err
+		}
+	}
+	os.Remove(phasefile)
+
+	return nil
+}
