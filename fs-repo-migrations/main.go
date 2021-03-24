8074d20bc79266ed8746d4d8093489757010a2aa
gammazero <gammazero@users.noreply.github.com>
2021-03-24 15:49:44 -0700

    'The "-to" flag defaults to "latest"'
    - Add check that the migration version requested is not greater than the latest.

--- fs-repo-migrations/main.go
+++ fs-repo-migrations/main.go
@@ -5,11 +5,13 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/ipfs/go-ipfs/repo/fsrepo/migrations"
 )
 
+// When searching for latest migration, start looking using this repo version
 const currentVersion = 11
 
 func yesNoPrompt(prompt string) bool {
@@ -40,40 +42,54 @@ func createFetcher(distPath string) migrations.Fetcher {
 }
 
 func main() {
-	var distPath string
-	target := flag.Int("to", currentVersion, "specify version to upgrade to")
-	yes := flag.Bool("y", false, "answer yes to all prompts")
-	version := flag.Bool("v", false, "print latest migrationavailable and exit")
+	distPath := flag.String("distpath", "", "specify the distributions build to use")
 	revertOk := flag.Bool("revert-ok", false, "allow running migrations backward")
-	flag.StringVar(&distPath, "distpath", "", "specify the distributions build to use")
+	targetStr := flag.String("to", "latest", "repo version to upgrade to, or \"latest\" for latest repo version")
+	version := flag.Bool("v", false, "print latest migration available and exit")
+	yes := flag.Bool("y", false, "answer yes to all prompts")
 	flag.Parse()
 
-	fetcher := createFetcher(distPath)
+	fetcher := createFetcher(*distPath)
 
-	var err error
+	var (
+		err    error
+		target int
+	)
+
 	if *version {
-		ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-		defer cancel()
-
-		var latestMigration int
-		for i := currentVersion; err == nil; i++ {
-			dist := fmt.Sprintf("fs-repo-%d-to-%d", i-1, i)
-			_, err = migrations.LatestDistVersion(ctx, fetcher, dist, false)
-			if err == nil {
-				latestMigration = i
-			}
-		}
-		if latestMigration == 0 {
-			fmt.Fprintln(os.Stderr, "Could not get available repo migrations:", err)
+		target, err = latestRepoMigration(fetcher)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
 		}
-		fmt.Println(latestMigration)
+		fmt.Println(target)
 		return
 	}
 
-	if *target == 0 {
+	switch *targetStr {
+	case "":
 		fmt.Fprintln(os.Stderr, "Please specify a target version to migrate the repo to")
 		os.Exit(1)
+	case "latest":
+		target, err = latestRepoMigration(fetcher)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	default:
+		target, err = strconv.Atoi(*targetStr)
+		if err != nil || target < 1 {
+			fmt.Fprintln(os.Stderr, "invalid target version, \"to\" must be a positive integer")
+			os.Exit(1)
+		}
+		// Check that target is not greater than latest version.  Ignore error
+		// here since may be offline and all migrations already in PATH.
+		latest, _ := latestRepoMigration(fetcher)
+		if latest != 0 && target > latest {
+			fmt.Fprintln(os.Stderr, "migration version", target, "does not exist, latest is", latest)
+			os.Exit(1)
+		}
+
 	}
 
 	vnum, err := migrations.RepoVersion("")
@@ -82,26 +98,45 @@ func main() {
 		os.Exit(1)
 	}
 
-	if vnum > *target && !*revertOk {
+	if vnum > target && !*revertOk {
 		fmt.Fprintln(os.Stderr, "ipfs migration: attempt to run backward migration\nTo allow, run this command again with --revert-ok")
 		os.Exit(1)
 	}
 
-	if vnum == *target {
-		fmt.Fprintln(os.Stderr, "ipfs migration: already at target version number")
+	if vnum == target {
+		fmt.Fprintln(os.Stderr, "ipfs migration: already at version", target)
 		return
 	}
 
 	ipfsDir, _ := migrations.IpfsDir("")
 	fmt.Printf("Found fs-repo version %d at %s\n", vnum, ipfsDir)
-	prompt := fmt.Sprintf("Do you want to upgrade this to version %d? [y/n]", *target)
+	prompt := fmt.Sprintf("Do you want to upgrade this to version %d? [y/n]", target)
 	if !(*yes || yesNoPrompt(prompt)) {
 		os.Exit(1)
 	}
 
-	err = migrations.RunMigration(context.Background(), fetcher, *target, "", *revertOk)
+	err = migrations.RunMigration(context.Background(), fetcher, target, "", *revertOk)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ipfs migration: ", err)
 		os.Exit(1)
 	}
+}
+
+func latestRepoMigration(fetcher migrations.Fetcher) (int, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	var latestMigration int
+	var err error
+	for i := currentVersion; err == nil; i++ {
+		dist := fmt.Sprintf("fs-repo-%d-to-%d", i-1, i)
+		_, err = migrations.LatestDistVersion(ctx, fetcher, dist, false)
+		if err == nil {
+			latestMigration = i
+		}
+	}
+	if latestMigration == 0 {
+		return 0, fmt.Errorf("Could not get available repo migrations: %s", err)
+	}
+	return latestMigration, nil
 }
